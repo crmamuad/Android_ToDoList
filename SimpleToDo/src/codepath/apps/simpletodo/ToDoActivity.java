1a0b7d2c77390f016d278cbf496e4b66c056f224
crmamuad <crmamuad@gmail.com>
2014-05-04 21:53:47 -0700

    removing unnecessary import.

--- SimpleToDo/src/codepath/apps/simpletodo/ToDoActivity.java
+++ SimpleToDo/src/codepath/apps/simpletodo/ToDoActivity.java
@@ -10,7 +10,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
-import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
